cfceb0a9030a958e9c4d62c229a542f799ad2366
tim ichiba <ichibat@gmail.com>
2019-08-04 22:19:13 +0900

    mini modify

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,11 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        testGCP2
-      </h1>
-      <h2 class="subtitle">
-        test GCP
-      </h2>
+      <h1 class="title for GCP">testGCP2</h1>
+      <h2 class="subtitle">test GCP</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
